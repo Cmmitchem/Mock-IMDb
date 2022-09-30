af35299bd805a5c0a6f4eebbf99099de721ae2d6
Cmmitchem <c.m.mitchem@tcu.edu>
2022-09-30 10:09:49 -0500

    json brings up images, and search bar functionality added

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
 const apiBaseUrl = "https://api.themoviedb.org/3";
 const apiKey = "9124a0e1d58a5aa17a09d4d274dd8fec";
-const imageBaseUrl = "https://image.tmdb.org/t/p/w500/";
-let image;
-const imageApiUrl = "https://api.themoviedb.org/3/configuration?api_key=9124a0e1d58a5aa17a09d4d274dd8fec";
+const imageBaseUrl = 'https://image.tmdb.org/t/p/w300';
 
+const moviesGrid = document.getElementById("movies-grid");
+const searchInput = document.getElementById("search-input");
+const searchForm = document.getElementById("search-form");
 
 async function fetchMoviesNowPlaying() {
     const response = await fetch(`${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`);
     const jsonResponse = await response.json();
-    console.log(jsonResponse);
-
-    const configuration = await fetch(`${apiBaseUrl}/configuration?api_key=${apiKey}`);
-    const config = await configuration.json();
+    const movies = jsonResponse.results;
     
-    console.log(config);
-   
 
-    const imsize = config.poster_sizes; 
-    const IMbase_url = config.base_url;
-    const poster =jsonResponse.results.poster_path;
-    ;
-
-    const images =await fetch(`${imageBaseUrl}${poster}`);
-    const imageResp = await images.json();
-    console.log(imageResp);
-
-
+    displayMovies(movies);
 }
-// async function fetchMovieImages(){
-//     const configuration = await fetch(`${apiBaseUrl}/configuration?api_key=${apiKey}`);
-//     const config = await configuration.json();
-    
-//     console.log(config);
-   
 
-//     const imsize = config.size; 
-//     const IMbase_url = config.base_url;
+async function searchMovies(query){
+    const response = await fetch(`${apiBaseUrl}/search/movie?api_key=${apiKey}&query="${query}"`);
+    const jsonResponse = await response.json();
+    const movies = jsonResponse.results;
 
-//     const images =await fetch(`${IMbase_url}${imsize}`)
-// }
+    displayMovies(movies);
+}
+
+function handleSearchFormSubmit(event){
+    event.preventDefault();
+    const searchQuery = searchInput.value;
+    const movies = searchMovies(searchQuery);
+    displayMovies(movies);
+}
 
 
+function displayMovies(movies) {
+    moviesGrid.innerHTML = movies.map(movie => 
+				`<div class="movie-card">
+	            <img src="${imageBaseUrl}${movie.poster_path}"/>
+	            <p>⭐${movie.vote_average}</p>
+	            <h1>${movie.title}</h1>
+	        </div>`
+    ).join("");
+}
 
+searchForm.addEventListener("submit", handleSearchFormSubmit);
 fetchMoviesNowPlaying();
+//searchMovies("Batman");
